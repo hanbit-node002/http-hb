f708db479d5636f4b9f0b55ec6526115cba9927a
user <a@a.com>
2017-08-26 18:06:28 +0900

    update routers

--- express.js
+++ express.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var server = express();
+
+server.use(morgan(':method :url'));
+server.use(express.static(__dirname + '/webRoot'));
 
 server.get('/', function(req, res) {
     res.send('Index');
@@ -8,6 +12,12 @@ server.get('/', function(req, res) {
 
 server.get('/hello', function(req, res) {
     res.send('Hello Node');
+});
+
+server.get('/user/:name', function(req, res) {
+    var name = req.params.name;
+
+    res.send('Hello ' + name);
 });
 
 server.listen(3000, function() {
